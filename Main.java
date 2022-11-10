e498cec945459a7ce90db2acd67912385068bdb6
tsunokake <r.tsunokake.sys22@morijyobi.ac.jp>
2022-11-10 10:30:29 +0900

    aaaaa

--- Main.java
+++ Main.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Main{
    public static void main(String[] args){
 	Scanner str = new Scanner(System.in);
-	System.out.println("身長");
-	double height = str.nextDouble();
-	System.out.println("体重");
-	double weight = str.nextDouble();
-
-	double bmi = weight/Math.pow(height,2);
+	System.out.println("身長:");
+	double aheight = str.nextDouble();
+	System.out.println("体重:");
+	double aweight = str.nextDouble();
+            
+	double abmi = aweight/(aheight*aheight);
 	System.out.println("BMI:");
-	System.out.println(String.format("%.2f",bmi));
+	System.out.println(String.format("%.2f",abmi));
+
+	System.out.orintln("身長:");
+	double bheight = str.nextDouble();
+	System.out.println("体重:");
+	double bweight = str.nextDouble();
+
+	double bbmi = bweight/(bheight*bheight);
+	System.out.println("BMI:");
+	System.out.println(String.format("%.2f",bbmi));
    }
 }
 
